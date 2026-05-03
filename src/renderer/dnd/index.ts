11ddf8734c405b3a5f709f96c7cdde21eac062bb
theMurfinator <brian@murfinator.com>
2026-05-03 15:38:33 -0400

    feat(dnd): add barrel export with css side-effect import

---
+++ src/renderer/dnd/index.ts
@@ -0,0 +1,10 @@
+import './dnd.css';
+
+export { SortableArea } from './SortableArea';
+export { SortableList } from './SortableList';
+export { SortableItem } from './SortableItem';
+export type { SortableItemRenderProps } from './SortableItem';
+export { DropIndicator } from './DropIndicator';
+export { DragPreviewCard } from './DragPreviewCard';
+export { useSortableSensors } from './useSortableSensors';
+export { useAutoScrollContainer } from './useAutoScrollContainer';
